5f53f47d1c865f4ad8f0fa2d2351d01f3f550c3d
CharlesSantos <charles-henrique2011@hotmail.com>
2018-09-18 11:23:59 -0300

    layout vending-machine

---
+++ src/app/vending-machine/vending-machine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vending-machine',
+  templateUrl: './vending-machine.component.html',
+  styleUrls: ['./vending-machine.component.css']
+})
+export class VendingMachineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
