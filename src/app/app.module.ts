33f64b849a88145a51f554c7a9b37713f9660485
Bruno Philippe <bruni.felipe@hotmail.com>
2018-10-02 22:41:30 -0300

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VendingMachineComponent } from './vending-machine/vending-machine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VendingMachineComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/vending-machine/vending-machine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vm-vending-machine',
+  templateUrl: './vending-machine.component.html',
+  styleUrls: ['./vending-machine.component.css']
+})
+export class VendingMachineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
